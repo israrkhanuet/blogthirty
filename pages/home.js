3b38fb92dfb7c554f1891d9e4980a3490eee3ece
israrkhanuet <israrkhanuet@gmail.com>
2023-03-31 18:20:33 +0500

    🚀 chore(package.json): add new dependencies Added new dependencies to the project, including body-parser, form-data, mongodb, mongoose, and next-connect.
    🎨 style(layout.js): change name variable value
    Changed the value of the name variable from "Israr khan" to "Bannu".
    
    🔧 chore(contact.js): add file read/write functionality
    Added functionality to read and write data to a JSON file named data.json. The data is read from the file, parsed, and then new data is added to it. The updated data is then converted back to a string and written back to the file.
    
    🚀 feat(create-job.js, mongo.js): add MongoDB integration for job creation
    🖼️ add(background.jpg): add background image for the website
    The create-job.js file now includes MongoDB integration for job creation. The handler function uses the MongoClient to connect to the MongoDB database and insert the job data into the mycollection collection. The mongo.js file exports a function getCollection that returns a collection from the mydatabase database. The background.jpg file is added to the project as the background image for the website.
    
    🎨 style(home.js): update homepage background image and text style
    The homepage background image has been updated to a more visually appealing image. The text style has also been updated to make it more prominent and readable. The code for fetching blogs and jobs data from API endpoints has been commented out as it is not currently being used.
    
    🔥 refactor(index.js): remove self-introduction and sample website text
    The self-introduction and sample website text have been removed from the index.js file. This improves the clarity of the code and removes unnecessary content.
    
    🎉 feat(jobs.js): add job posting form
    This commit adds a job posting form to the jobs page. The form allows users to input job details such as title, description, salary, and image. The form data is submitted to the server via a POST request to the /api/create-job endpoint. The form also includes basic styling using CSS modules.
    
    💄 style(JobForm.module.css): add styles for JobForm component
    This commit adds styles for the JobForm component. The styles include a flexbox layout for the form, bold labels, and a green submit button with hover effect. The styles are defined in a new CSS file named JobForm.module.css.

--- pages/home.js
+++ pages/home.js
@@ -1,106 +1,138 @@
 import Head from "next/head";
-import Link from "next/link";
-import { useState, useEffect } from "react";
 
-function Home() {
-  const [blogs, setBlogs] = useState([]);
-  const [jobs, setJobs] = useState([]);
+// import Link from "next/link";
+// import { useState, useEffect } from "react";
 
-  useEffect(() => {
-    // Fetch blogs and jobs data from API endpoints
-    const fetchBlogs = async () => {
-      const res = await fetch("/api/blogs");
-      const data = await res.json();
-      setBlogs(data);
-    };
-    const fetchJobs = async () => {
-      const res = await fetch("/api/jobs");
-      const data = await res.json();
-      setJobs(data);
-    };
-    fetchBlogs();
-    fetchJobs();
-  }, []);
-
+const HomePage = () => {
   return (
-    <div className="container">
+    <div>
       <Head>
-        <title>My App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>My Next.js App</title>
       </Head>
-
       <main>
-        <h1>Welcome blogthirty</h1>
-
-        <section>
-          <h2>Blogs</h2>
-          {blogs.length > 0 ? (
-            <ul>
-              {blogs.map((blog) => (
-                <li key={blog.id}>
-                  <Link href={`/blogs/${blog.id}`}>
-                    <a>{blog.title}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No blogs found.</p>
-          )}
-        </section>
-
-        <section>
-          <h2>Jobs</h2>
-          {jobs.length > 0 ? (
-            <ul>
-              {jobs.map((job) => (
-                <li key={job.id}>
-                  <Link href={`/jobs/${job.id}`}>
-                    <a>{job.title}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No jobs found.</p>
-          )}
-        </section>
+        <h1>Welcome to Bannu</h1>
       </main>
-
       <style jsx>{`
-        .container {
-          max-width: 800px;
-          margin: 0 auto;
-          padding: 2rem;
+        main {
+          background-image: url("https://free-images.com/lg/da3b/lake_minnewanka_11092005.jpg");
+          background-size: cover;
+          background-position: center;
+          height: 100vh;
+          display: flex;
+          justify-content: center;
+          align-items: center;
         }
         h1 {
-          margin-bottom: 2rem;
-          text-align: center;
-        }
-        section {
-          margin-bottom: 3rem;
-        }
-        h2 {
-          margin-bottom: 1rem;
-        }
-        ul {
-          list-style: none;
-          padding: 0;
-          margin: 0;
-        }
-        li {
-          margin-bottom: 0.5rem;
-        }
-        a {
-          color: #6c63ff;
-          text-decoration: none;
-        }
-        a:hover {
-          text-decoration: underline;
+          font-size: 48px;
+          color: white;
+          text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
         }
       `}</style>
     </div>
   );
-}
+};
 
-export default Home;
+export default HomePage;
+
+// function Home() {
+//   const [blogs, setBlogs] = useState([]);
+//   const [jobs, setJobs] = useState([]);
+
+//   useEffect(() => {
+//     // Fetch blogs and jobs data from API endpoints
+//     const fetchBlogs = async () => {
+//       const res = await fetch("/api/blogs");
+//       const data = await res.json();
+//       setBlogs(data);
+//     };
+//     const fetchJobs = async () => {
+//       const res = await fetch("/api/jobs");
+//       const data = await res.json();
+//       setJobs(data);
+//     };
+//     fetchBlogs();
+//     fetchJobs();
+//   }, []);
+
+//   return (
+//     <div className="container">
+//       <Head>
+//         <title>My App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main>
+//         <h1>Welcome blogthirty</h1>
+
+//         <section>
+//           <h2>Blogs</h2>
+//           {blogs.length > 0 ? (
+//             <ul>
+//               {blogs.map((blog) => (
+//                 <li key={blog.id}>
+//                   <Link href={`/blogs/${blog.id}`}>
+//                     <a>{blog.title}</a>
+//                   </Link>
+//                 </li>
+//               ))}
+//             </ul>
+//           ) : (
+//             <p>No blogs found.</p>
+//           )}
+//         </section>
+
+//         <section>
+//           <h2>Jobs</h2>
+//           {jobs.length > 0 ? (
+//             <ul>
+//               {jobs.map((job) => (
+//                 <li key={job.id}>
+//                   <Link href={`/jobs/${job.id}`}>
+//                     <a>{job.title}</a>
+//                   </Link>
+//                 </li>
+//               ))}
+//             </ul>
+//           ) : (
+//             <p>No jobs found.</p>
+//           )}
+//         </section>
+//       </main>
+
+//       <style jsx>{`
+//         .container {
+//           max-width: 800px;
+//           margin: 0 auto;
+//           padding: 2rem;
+//         }
+//         h1 {
+//           margin-bottom: 2rem;
+//           text-align: center;
+//         }
+//         section {
+//           margin-bottom: 3rem;
+//         }
+//         h2 {
+//           margin-bottom: 1rem;
+//         }
+//         ul {
+//           list-style: none;
+//           padding: 0;
+//           margin: 0;
+//         }
+//         li {
+//           margin-bottom: 0.5rem;
+//         }
+//         a {
+//           color: #6c63ff;
+//           text-decoration: none;
+//         }
+//         a:hover {
+//           text-decoration: underline;
+//         }
+//       `}</style>
+//     </div>
+//   );
+// }
+
+// export default Home;
--- pages/jobs.js
+++ pages/jobs.js
@@ -1,60 +1,129 @@
-import Head from "next/head";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import styles from "../styles/JobForm.module.css";
+import Layout from "../components/layout";
 
-function Jobs() {
-  const [job, setJob] = useState(null);
+export default function MyForm() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    salary: "",
+    image: null,
+  });
 
-  useEffect(() => {
-    // Fetch job data from API endpoint
-    const fetchJob = async () => {
-      const res = await fetch("/api/jobs/today");
-      const data = await res.json();
-      setJob(data);
-    };
-    fetchJob();
-  }, []);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("/api/create-job", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      console.log(response.collection);
+      // Handle successful response
+    } else {
+      // Handle error response
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value, files } = event.target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: files ? files[0] : value,
+    }));
+  };
 
   return (
-    <div className="container">
-      <Head>
-        <title>Job Post - My Next.js App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Layout>
+        <form onSubmit={handleSubmit} className={styles.form2}>
+          <h4>Advertise Your Jobs Here</h4>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              className={styles.input1}
+              type="text"
+              name="title"
+              value={formData.title}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Descriptions</label>
+            <textarea
+              name="description"
+              className={styles.input2}
+              value={formData.description}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Salary</label>
+            <input
+              className={styles.input1}
+              type="number"
+              name="salary"
+              value={formData.salary}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Picture</label>
+            <input
+              className={styles.button1}
+              type="file"
+              name="image"
+              onChange={handleInputChange}
+            />
+          </div>
 
-      <main>
-        <h1>Job Post</h1>
+          <button type="submit" className={styles.button1}>
+            Submit
+          </button>
+        </form>
+        <style jsx>{`
+          form {
+            display: flex;
+            flex-direction: column;
+            max-width: 500px;
+            margin: 0 auto;
+          }
 
-        {job ? (
-          <section>
-            <h2>{job.title}</h2>
-            <p>{job.description}</p>
-            <p>{job.location}</p>
-            <p>{job.salary}</p>
-          </section>
-        ) : (
-          <p>No job post found for today.</p>
-        )}
-      </main>
+          input,
+          textarea {
+            margin-bottom: 1rem;
+            padding: 0.5rem;
+            border-radius: 4px;
+            border: 1px solid #ccc;
+            font-size: 1rem;
+          }
 
-      <style jsx>{`
-        .container {
-          max-width: 800px;
-          margin: 0 auto;
-          padding: 2rem;
-        }
-        h1 {
-          margin-bottom: 2rem;
-          text-align: center;
-        }
-        h2 {
-          margin-bottom: 1rem;
-        }
-        p {
-          margin-bottom: 0.5rem;
-        }
-      `}</style>
-    </div>
+          input[type="number"] {
+            -moz-appearance: textfield;
+          }
+
+          input[type="file"] {
+            margin-bottom: 2rem;
+          }
+
+          button[type="submit"] {
+            background-color: #0077cc;
+            color: #fff;
+            border: none;
+            border-radius: 4px;
+            padding: 0.5rem 1rem;
+            font-size: 1rem;
+            cursor: pointer;
+            transition: background-color 0.2s ease-in-out;
+          }
+
+          button[type="submit"]:hover {
+            background-color: #005fa3;
+          }
+        `}</style>
+      </Layout>
+    </>
   );
 }
-
-export default Jobs;
