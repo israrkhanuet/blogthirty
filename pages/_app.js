568a749f30cb4421cf29dbc3059e1328ebc1dc26
israrkhanuet <israrkhanuet@gmail.com>
2024-01-09 12:37:34 +0500

    analyticComp

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/global.css";
 import Navbar from "../components/navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />;
+      <Analytics />
     </>
   );
 }
